93f9a788c8854891720c8c22b97a3af92f0184ea
Héc Trần <86024288+hectran12@users.noreply.github.com>
2023-06-12 21:03:43 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Library published by HexTran"
+
+if ! command -v python &> /dev/null ; then
+    echo "Python is not installed"
+    exit 1
+fi
+
+# pip upgrade
+python -m pip install --upgrade pip
+# install pygetwindow
+echo "Installing pygetwindow"
+pip install pygetwindow
+# install pyautogui
+echo "Installing pyautogui"
+pip install pyautogui
+# install opencv-python
+echo "Installing opencv-python"
+pip install opencv-python
+# install discord
+echo "Installing discord"
+pip install discord
+
+# end
+echo "Setup completed"
+echo "Thanks for using hexTran's products"
+read -p "Press any key to continue..." var
